91f9c59a8332881560da602ba9bf715e194872eb
aurelio973 <sadiomaneorw442@gmail.com>
2025-08-31 04:00:04 +0800

    Update main.go

--- go/task2/main.go
+++ go/task2/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 // 指针
-// 1.题目 ：编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值
+// 1.编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值
 func add(num *int) {
 	*num += 10
 }
@@ -47,4 +47,43 @@ func main() {
 	go odds()
 	go evens()
 	time.Sleep(100 * time.Millisecond)
+}
+
+// 2.设计一个任务调度器，接收一组任务（可以用函数表示），并使用协程并发执行这些任务，同时统计每个任务的执行时间
+func main() {
+	// 任务1：计算1到100000的总和
+	task1 := func() {
+		sum := 0
+		for i := 1; i <= 100000; i++ {
+			sum += i
+		}
+		fmt.Println("任务1结果：", sum)
+	}
+	// 任务2：计算1到50000的平方和
+	task2 := func() {
+		sum := 0
+		for i := 1; i <= 50000; i++ {
+			sum += i * i
+		}
+		fmt.Println("任务2结果：", sum)
+	}
+	ch := make(chan bool, 2)
+
+	// 启动任务1
+	go func() {
+		start := time.Now()
+		task1()
+		fmt.Println("任务1耗时：", time.Since(start))
+		ch <- true
+	}()
+
+	// 启动任务2
+	go func() {
+		start := time.Now()
+		task2()
+		fmt.Println("任务2耗时：", time.Since(start))
+		ch <- true
+	}()
+	<-ch
+	<-ch
 }
