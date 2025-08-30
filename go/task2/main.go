e787a2ac29310408c3ea7a9e09f0d9780069038e
aurelio973 <sadiomaneorw442@gmail.com>
2025-08-31 05:16:15 +0800

    Update main.go

--- go/task2/main.go
+++ go/task2/main.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "time"
   "math"
+  "sync/automic"
 )
 
 // 指针
@@ -210,3 +211,51 @@ func main() {
 
 // 锁机制
 // 1.编写一个程序，使用 sync.Mutex 来保护一个共享的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值
+type SafeCounter struct {
+	mu    sync.Mutex
+	count int
+}
+
+// 增加计数
+func (c *SafeCounter) Increment() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
+
+// 获取当前计数
+func (c *SafeCounter) GetCount() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.count
+}
+
+func main() {
+	counter := SafeCounter{}
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				counter.Increment()
+			}
+		}()
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Printf("Final count: %d\n", counter.GetCount())
+}
+
+// 2.使用原子操作（ sync/atomic 包）实现一个无锁的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值
+func main() {
+	var count int64
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				atomic.AddInt64(&count, 1) 
+			}
+		}()
+	}
+
+	time.Sleep(500 * time.Millisecond)
+	fmt.Printf("Final count: %d\n", atomic.LoadInt64(&count))
+}
