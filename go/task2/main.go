ebf554015f1106848b5abee27bfda7290b8d8265
aurelio973 <sadiomaneorw442@gmail.com>
2025-08-31 04:41:08 +0800

    Update main.go

--- go/task2/main.go
+++ go/task2/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "time"
+  "math"
 )
 
 // 指针
@@ -87,3 +88,99 @@ func main() {
 	<-ch
 	<-ch
 }
+
+// 面向对象
+// 1.定义一个 Shape 接口，包含 Area() 和 Perimeter() 两个方法。然后创建 Rectangle 和 Circle 结构体，实现 Shape 接口。在主函数中，创建这两个结构体的实例，并调用它们的 Area() 和 Perimeter() 方法
+type Shape interface {
+	Area()       float64
+	Perimeter()  float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+type Circle struct {
+	radius float64
+}
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func main() {
+	rect := Rectangle{
+		width:  5.5,
+		height: 3.2,
+	}
+
+	circle := Circle{
+		radius: 4.6,
+	}
+
+	fmt.Println("矩形面积：", rect.Area())
+	fmt.Println("矩形周长：", rect.Perimeter())
+
+	fmt.Println("圆形面积：", circle.Area())
+	fmt.Println("圆形周长：", circle.Perimeter())
+}   
+
+// 2.使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Employee struct {
+	EmployeeID int
+	Person
+}
+
+func (e Employee) PrintInfo() {
+	fmt.Printf("ID: %d, 姓名: %s, 年龄: %d\n", e.EmployeeID, e.Name, e.Age)
+}
+
+func main() {
+	info := Employee{
+		Person: Person{
+			Name: "张三",
+			Age:  18,
+		},
+		EmployeeID: 101,
+	}
+
+	info.PrintInfo()
+}
+
+// Channel
+// 1.编写一个程序，使用通道实现两个协程之间的通信。一个协程生成从1到10的整数，并将这些整数发送到通道中，另一个协程从通道中接收这些整数并打印出来
+func main() {
+	ch := make(chan int)
+	go func() {
+		for i := 1; i <= 10; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	go func() {
+		for num := range ch {
+			fmt.Printf("%d ", num)
+		}
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("\n程序结束")
+}
+
+
+// 2.实现一个带有缓冲的通道，生产者协程向通道中发送100个整数，消费者协程从通道中接收这些整数并打印
