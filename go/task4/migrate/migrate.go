4ab74e7e88d83bfc9d12e7aae9a2a23a0eb7d32a
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:13:59 +0800

    Update migrate.go

--- go/task4/migrate/migrate.go
+++ go/task4/migrate/migrate.go
@@ -1,1 +1,35 @@
+package migrate
 
+import (
+	"blog/internal/mysqldb"
+
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+
+	"blog/models"
+	"blog/zaplogger"
+)
+
+func InitMigrate() *gorm.DB {
+	// 初始化日志模块
+	logger := zaplogger.InitLogger()
+	logger.Debug("初始化日志模块成功")
+	// 初始化博客数据库模型，进行ORM映射生成表结构
+	// 连接数据库
+	db,err := mysqldb.InitDB()
+	if err != nil {
+		logger.Error("error",zap.Error(err))
+		panic(err)
+		return nil
+	}
+	logger.Debug("连接数据库成功，开始迁移")
+	// 连接数据库成功，开始迁移
+	err = db.AutoMigrate(&models.User{},&models.Post{},&models.Comment{})
+	if err != nil {
+		logger.Error("迁移数据库error",zap.Error(err))
+		panic(err)
+		return nil
+	}
+	logger.Debug("数据迁移成功，迁移表结构：",zap.String("tables","User,Post,Comment"))
+	return db
+}
