c1509ce253267c55821aa273e7ba120b38e25c04
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:12:29 +0800

    Update latency.go

--- go/task4/middlewares/latency.go
+++ go/task4/middlewares/latency.go
@@ -1,1 +1,44 @@
+package middlewares
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+// Logger中间件配置
+// LatencyLogger 创建一个记录请求耗时得中间件
+func LatencyLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 记录请求开始时间
+		start := time.Now()
+
+		// 在处理请求之前可以记录一些信息
+		fmt.Printf("[%s]%s %s - Request started\n",
+	    	start.Format("2025-09-18 17:20:35"),
+			c.Request.Method,
+			c.Request.URL.Path)
+		// 在中间件中添加调试信息
+		c.Set("requestID",uuid.NewString())
+		log.Printf("[%s]%s %s",c.GetString("requestID"),c.Request.Method,c.Request.URL)
+		// 处理请求
+		c.Next()
+
+		// 计算耗时
+		latency := time.Since(start)
+
+		// 获取响应状态码
+		status := c.Writer.Status()
+
+		// 记录请求完成信息，包括耗时
+		fmt.Printf("[%s] %s %s - Completed in %v with status %d\n",
+			time.Now().Format("2025-09-18 17:20:35"),
+			c.Request.Method,
+			c.Request.URL.Path,
+			latency,
+			status)
+	}
+}
