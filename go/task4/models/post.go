7e9024384a1f39df7a4e49562c91896a57ae0264
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:15:23 +0800

    Update post.go

--- go/task4/models/post.go
+++ go/task4/models/post.go
@@ -1,1 +1,13 @@
+package models
 
+import "gorm.io/gorm"
+
+// 文章模型
+type Post struct {
+	gorm.Model //自动补全ID，CreateAt,UpdateAt,Delete软删除字段
+	ID         uint64
+	Title      string `gorm:"not null"`          //标题
+	Content    string `gorm:"not null"`          //内容
+	UserID     uint64 `gorm:"not null"`          //与User表一对多关系，一个用户可以发多条推文
+	User       User   `gorm:"foreignkey:UserID"` //表明外键为UserID，与User表的ID进行关联
+}
