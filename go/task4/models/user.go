eac9c95940216f020ebdb8afd7fec67023cbb47b
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:16:04 +0800

    Update user.go

--- go/task4/models/user.go
+++ go/task4/models/user.go
@@ -1,1 +1,12 @@
+package models
 
+import "gorm.io/gorm"
+
+// 用户模型
+type User struct {
+	ID         uint64 `gorm:"primaryKey" json:"id"`
+	gorm.Model        //自动补全ID，CreateAt,UpdateAt,Delete软删除字段
+	Username   string `gorm:"unique;not null"` //用户名，账号
+	Password   string `gorm:"not null"`        //密码
+	Email      string `gorm:"unique;not null"` //邮箱
+}
