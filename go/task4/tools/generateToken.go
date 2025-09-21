9ea27c0a9cca89a6619bb86e37501db35fefec82
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:21:58 +0800

    Update generateToken.go

--- go/task4/tools/generateToken.go
+++ go/task4/tools/generateToken.go
@@ -1,1 +1,22 @@
+package tools
 
+import (
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// 生成JWT令牌
+type MyClaims struct {
+	UserId        uint64   `json:"userId"`
+	Username      string   `json:"username"`
+	Roles         []string `json:"roles"`
+	Exp           int64    `json:"exp"`
+	jwt.MapClaims          //这个是用来继承MapClaims的实现
+}
+
+// 生成加密串
+func GenerateToken(claims *MyClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("SIGNATURE_KEY")))
+}
