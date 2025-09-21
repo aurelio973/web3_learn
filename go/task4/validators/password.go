a07cb09a27edcf084199eac5437590f36fe2b595
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:24:51 +0800

    Update password.go

--- go/task4/validators/password.go
+++ go/task4/validators/password.go
@@ -1,1 +1,17 @@
+package validators
 
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// 自定义绑定密码验证规则，使用正则表达
+
+var PasswordValidator validator.Func = func(fl validator.FieldLevel) bool {
+	data,ok := fl.Field().Interface().(string)
+	if ok {
+		return regexp.MustCompile(`^([0-9].*[a-zA-Z].*[0-9]).*$`).MatchString(data)
+	}
+	return ok
+}
