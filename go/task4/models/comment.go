bd7393300161b77610f5cb7ef826b4eae8ec9c5f
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:14:38 +0800

    Update comment.go

--- go/task4/models/comment.go
+++ go/task4/models/comment.go
@@ -1,1 +1,14 @@
+package models
 
+import "gorm.io/gorm"
+
+// 评论模型
+type Comment struct {
+	gorm.Model        //自动补全ID，CreateAt,UpdateAt,Delete软删除字段
+	ID         uint64 `gorm:"primaryKey;autoIncrement"`
+	Content    string `gorm:"not null"`
+	UserID     uint64 `gorm:"not null"` //与User用户表一对多关系，一个用户下的推文有多条评论
+	User       User   `gorm:"foreignkey:UserID"`
+	PostID     uint64 `gorm:"not null"` //与Post推文表一对多关系，一个推文下有多条评论
+	Post       Post   `gorm:"foreignkey:PostID"`
+}
