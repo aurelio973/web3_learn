0053267a0fb0ef19705deceb9a16cd94b6a675ce
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-21 22:16:56 +0800

    Update comment_repository.go

--- go/task4/repository/comment_repository.go
+++ go/task4/repository/comment_repository.go
@@ -1,1 +1,23 @@
+package repository
+
+import (
+	"blog/models"
+
+	"gorm.io/gorm"
+)
+
+type CommentRep struct {
+	Db *gorm.DB
+}
+
+// 对指定文章进行评论
+func (c *CommentRep) CreateComment(comment *models.Comment) error {
+	// 移除多余的Where条件，直接插入评论
+	return c.Db.Create(comment).Error
+}
+
+// 根据文章ID获取所有的评论列表
+func (c *CommentRep) QueryCommentByPostId(comment *models.Comment, commentResults *[]models.Comment) error {
+	return c.Db.Debug().Model(comment).Where("post_id=?", comment.PostID).Find(commentResults).Error
+}
 
