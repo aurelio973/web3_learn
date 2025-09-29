ef19466884e2500381c9260a674296930d865182
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-30 02:31:48 +0800

    Update Voting.sol

--- solidity/task1/Voting.sol
+++ solidity/task1/Voting.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
 
+contract Voting {
+    mapping (string => uint256) private votes;
+    string[] private candidates;
+
+    // 投票
+    function vote(string memory candidate) public {
+        if (votes[candidate] == 0) {
+        candidates.push(candidate);
+    }
+        votes[candidate]+=1;
+    }
+
+    // 获取票数
+    function getVotes(string memory candidate) public view returns (uint256) {
+        return votes[candidate];
+    }
+
+    // 重置票数
+    function resetVotes() public {
+        uint length = candidates.length;
+        for(uint i=0;i < length; i++) {
+            votes[candidates[i]] = 0;
+        }
+    }
+}
