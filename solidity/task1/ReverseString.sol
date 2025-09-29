c547d6e517e457c417e35d55eee9014de8f26462
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-30 02:44:39 +0800

    Update ReverseString.sol

--- solidity/task1/ReverseString.sol
+++ solidity/task1/ReverseString.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
 
+contract ReverseString {
+    function reverse(string memory _str) public pure returns (string memory) {
+        bytes memory strBytes = bytes(_str);
+        bytes memory reversed = new bytes(strBytes.length);
+        
+        for (uint i = 0; i < strBytes.length; i++) {
+            reversed[i] = strBytes[strBytes.length - 1 - i];
+        }
+        
+        return string(reversed);
+    }
+}
+    
