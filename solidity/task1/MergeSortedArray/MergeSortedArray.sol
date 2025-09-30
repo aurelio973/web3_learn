7c5e752a4f813802baf0b3c5c6361912ab4cebce
aurelio973 <sadiomaneorw442@gmail.com>
2025-09-30 14:31:39 +0800

    Update MergeSortedArray.sol

--- solidity/task1/MergeSortedArray/MergeSortedArray.sol
+++ solidity/task1/MergeSortedArray/MergeSortedArray.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
 
+contract MergeSortedArray {
+    function merge(uint[] memory arr1, uint[] memory arr2) public pure returns(uint[] memory) {
+        uint[] memory result = new uint[](arr1.length+arr2.length);
+        uint i = 0;
+        uint j = 0;
+        uint k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if(arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < arr1.length) {
+            result[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < arr2.length) {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }
+        return result;
+
+     }
+}
